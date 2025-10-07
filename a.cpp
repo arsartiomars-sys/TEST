617fd2f9e2e9368ae2ba01087080e4288557a460
Artiom <arsartiomars@gmail.com>
2025-10-07 20:40:01 +0300

    Jopa

---
+++ a.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+int main(){
+	std::cout << "ЗДРАВСТВУЙТЕ, АНТОН. У МЕНЯ ПОЛУЧИЛОСЬ УСТАНОВИТЬ КОМПИЛЯТОР. "<<std::endl;
+	std::cout << "МОЖНА 10 ПОЖАЛУЙСТА";
+	char ch = 0;
+	std::cin.get(ch);
+}
